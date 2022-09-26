32fbda6d3faa9f03d78c49264a18c57fd2adb089
VictoriaBecher <victoria_enache@ymail.com>
2022-09-26 21:49:42 +0300

    test

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main className="text-3xl font-bold underline">homepage</main>
+    </>
+  );
+};
+
+export default Home;
