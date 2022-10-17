ed1620648d0dd825d56cc081b160142d5d3a41c2
VictoriaBecher <victoria_enache@ymail.com>
2022-10-17 19:23:07 +0300

    update cart

--- src/components/cart/CartControl.jsx
+++ src/components/cart/CartControl.jsx
@@ -1,14 +1,17 @@
 import Link from 'next/link';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 
-export const CartControl = ({ cart }) => {
-  // const { products } = cart;
-  // const cartQty = products.reduce((cartQty, product) => {
-  //   const { quantity } = product;
-  //   cartQty += quantity;
+export const CartControl = ({ cart = null }) => {
+  if (cart === null) {
+    return <></>;
+  }
+  const { products } = cart;
+  const cartQty = products.reduce((cartQty, product) => {
+    const { quantity } = product;
+    cartQty += quantity;
 
-  //   return cartQty;
-  // }, 0);
+    return cartQty;
+  }, 0);
 
   return (
     <ul className="border border-zinc-400">
@@ -18,7 +21,7 @@ export const CartControl = ({ cart }) => {
             className="w-24 h-24 flex justify-center items-center hover:text-zinc-900 hover:bg-zinc-300"
             title="Cart"
           >
-            {/* {cartQty} */}
+            {cartQty}
             <AiOutlineShoppingCart size="32"></AiOutlineShoppingCart>
           </a>
         </Link>
