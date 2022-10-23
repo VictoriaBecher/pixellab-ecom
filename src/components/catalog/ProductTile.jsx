0c705bff55fd224d552245ba5241ba5b9a002bee
VictoriaBecher <victoria_enache@ymail.com>
2022-10-23 17:46:00 +0300

    update

--- src/components/catalog/ProductTile.jsx
+++ src/components/catalog/ProductTile.jsx
@@ -10,7 +10,7 @@ export const ProductTile = ({ product }) => {
 
   return (
     <article className="w-full">
-      <header>
+      <header className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
         <div className="w-full text-center">
           <Link href={`/products/${id}`}>
             <a title={title}>
